b845ecd58f60e95d89e74797dd99a4d7b04b1908
NasirAsadov <asadovnasir707@gmail.com>
2024-08-31 03:52:35 +0400

    Portifolio Website

---
+++ Portifolio-Website/js/main.js
@@ -0,0 +1,8 @@
+function OpenOverlay() {
+    var element = document.getElementById("overlay");
+    element.classList.add("active");
+  }
+function CloseOverlay() {
+    var element = document.getElementById("overlay");
+    element.classList.remove("active");
+  }
