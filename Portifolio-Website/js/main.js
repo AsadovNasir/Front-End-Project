2b8fc59d1e58a4552be3ac39e17b91446f1a5b2f
AsadovNasir <140751769+AsadovNasir@users.noreply.github.com>
2024-09-01 16:53:55 +0400

    Delete Portifolio-Website directory

--- Portifolio-Website/js/main.js
+++
@@ -1,8 +0,0 @@
-function OpenOverlay() {
-    var element = document.getElementById("overlay");
-    element.classList.add("active");
-  }
-function CloseOverlay() {
-    var element = document.getElementById("overlay");
-    element.classList.remove("active");
-  }
